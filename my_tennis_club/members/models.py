13cc6dab233976bce94e96d37a0d2f8024ba63fe
Mbala Sadraque <orlandosadraquiorlandinho9@gmail.com>
2024-07-27 18:24:25 +0100

    adicionar novos campos

--- my_tennis_club/members/models.py
+++ my_tennis_club/members/models.py
@@ -4,4 +4,6 @@ class Member(models.Model):
 
     firstName = models.CharField(max_length=225)
     lastName = models.CharField(max_length=225)
+    phone  = models.IntegerField(null= True)
+    joined_date = models.DateField(null= True)
     
