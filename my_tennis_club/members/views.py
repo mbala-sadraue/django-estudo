12327ab2a2b96a8257981d78593a0260542c7a4c
Mbala Sadraque <orlandosadraquiorlandinho9@gmail.com>
2024-07-30 20:42:13 +0100

    d

--- my_tennis_club/members/views.py
+++ my_tennis_club/members/views.py
@@ -25,6 +25,5 @@ def details(request, id):
 
     template = loader.get_template("details.html")
     member = Member.objects.get(id= id)
-    # print(member)
     context = {"member":member}
     return HttpResponse(template.render(context, request))
