b00c1b8030423ba9bc9ae5bfb607bedf0c972fb6
Mbala Sadraque <orlandosadraquiorlandinho9@gmail.com>
2024-08-03 23:43:47 +0100

    mostrar o valor

--- my_tennis_club/my_tennis_club/urls.py
+++ my_tennis_club/my_tennis_club/urls.py
@@ -18,6 +18,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    # path('members/', include("members.urls"))
+    # path('admin/', admin.site.urls),
+    path('members/', include("members.urls"))
 ]
